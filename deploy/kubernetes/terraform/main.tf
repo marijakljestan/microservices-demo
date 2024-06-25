b4556dd95e53843d44b2ef29a14222d04a88d9ce
mkljestan <mkljestan@griddynamics.com>
2024-06-25 11:10:39 +0200

    refactor: use zonal instead of regional clusters

--- deploy/kubernetes/terraform/main.tf
+++ deploy/kubernetes/terraform/main.tf
@@ -48,7 +48,7 @@ resource "google_service_account" "gke-sa" {
 
 resource "google_container_cluster" "dev-cluster" {
   name                     = "dev-cluster"
-  location                 = "us-east1"
+  location                 = "us-east1-c"
   network = google_compute_network.vpc.self_link
   subnetwork = google_compute_subnetwork.dev-subnet.self_link
   remove_default_node_pool = true
@@ -57,7 +57,7 @@ resource "google_container_cluster" "dev-cluster" {
 
 resource "google_container_node_pool" "dev_preemptible_nodes" {
   name       = "dev-node-pool"
-  location   = "us-east1"
+  location   = "us-east1-c"
   cluster    = google_container_cluster.dev-cluster.name
   node_count = 1
 
@@ -74,7 +74,7 @@ resource "google_container_node_pool" "dev_preemptible_nodes" {
 
 resource "google_container_cluster" "stage-cluster" {
   name                     = "stage-cluster"
-  location                 = "us-west1"
+  location                 = "us-west1-c"
   network = google_compute_network.vpc.self_link
   subnetwork = google_compute_subnetwork.stage-subnet.self_link
   remove_default_node_pool = true
@@ -83,7 +83,7 @@ resource "google_container_cluster" "stage-cluster" {
 
 resource "google_container_node_pool" "stage_preemptible_nodes" {
   name       = "stage-node-pool"
-  location   = "us-west1"
+  location   = "us-west1-c"
   cluster    = google_container_cluster.stage-cluster.name
   node_count = 1
 
@@ -100,7 +100,7 @@ resource "google_container_node_pool" "stage_preemptible_nodes" {
 
 resource "google_container_cluster" "prod-cluster" {
   name                     = "prod-cluster"
-  location                 = "us-central1"
+  location                 = "us-central1-c"
   network = google_compute_network.vpc.self_link
   subnetwork = google_compute_subnetwork.prod-subnet.self_link
   remove_default_node_pool = true
@@ -109,7 +109,7 @@ resource "google_container_cluster" "prod-cluster" {
 
 resource "google_container_node_pool" "prod_preemptible_nodes" {
   name       = "prod-node-pool"
-  location   = "us-central1"  # TODO: use zone
+  location   = "us-central1-c"
   cluster    = google_container_cluster.prod-cluster.name
   node_count = 1
 
